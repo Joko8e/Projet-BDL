ebdb875dcefd3d2a80b24d77c2ed4230ea80d059
Jordy Mavuidi <j.mavuidi@gmail.com>
2025-11-19 17:57:15 +0100

    modif back et ajout Front

---
+++ FRONT-END/src/utils/constant/url.js
@@ -0,0 +1,19 @@
+const URL = {
+    
+    AUTH_SIGN: '',
+    AUTH_REGISTER:'',
+
+    POST_MARQUE: 'http://localhost:8000/api/marque/post',
+    GET_ALL_MARQUE: 'http://localhost:8000/api/marque/get',
+    GET_MARQUE_BY_ID: 'http://localhost:8000/api/marque/get',
+    UPDATE_MARQUE: 'http://localhost:8000/api/marque/put',
+    DELETE_MARQUE: 'http://localhost:8000/api/marque/delete',
+    
+    POST_PRODUIT: 'http://localhost:8000/api/chaussure/post',
+    GET_ALL_PRODUIT: 'http://localhost:8000/api/chaussure/get',
+    GET_PRODUIT_BY_ID: 'http://localhost:8000/api/chaussure/get',
+    UPDATE_PRODUIT: 'http://localhost:8000/api/chaussure/put',
+    DELETE_PRODUIT:'http://localhost:8000/api/chaussure/delete',
+}
+
+export default URL;
